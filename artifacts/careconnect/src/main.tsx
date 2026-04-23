7b183ecd9caabdc9b887119f0e8c9a923580860d
ShivaniDwivedi22 <shivani.dwivedi22@gmail.com>
2026-04-23 14:06:16 -0400

    Update main.tsx

--- artifacts/careconnect/src/main.tsx
+++ artifacts/careconnect/src/main.tsx
@@ -1,13 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
 
-// Initialize the client OUTSIDE the render cycle
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
